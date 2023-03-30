fd02512d9abaa9f18506eb24ca33d5029c765828
SimonWang9610 <dengpan9610.wang@gmail.com>
2023-03-30 11:42:32 -0400

    test gpt_tokenizer functions

--- packages/flutter_gpt_tokenizer/example/lib/main.dart
+++ packages/flutter_gpt_tokenizer/example/lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
 
-import 'package:flutter_gpt_tokenizer/flutter_gpt_tokenizer.dart'
-    as flutter_gpt_tokenizer;
+import 'package:flutter/material.dart';
+
+import 'package:flutter_gpt_tokenizer/flutter_gpt_tokenizer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,15 +17,32 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final TextEditingController _controller = TextEditingController();
+
+  BPEWrapper? tokenizer;
+
   @override
   void initState() {
     super.initState();
+    _initTokenizer();
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _initTokenizer() async {
+    tokenizer = await Tokenizer.getTokenizer("gpt-4");
+  }
+
+  Uint32List? _encoded;
+  String? _decoded;
+  int? _count;
+
+  @override
   Widget build(BuildContext context) {
-    const textStyle = TextStyle(fontSize: 25);
-    const spacerSmall = SizedBox(height: 10);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -35,16 +53,58 @@ class _MyAppState extends State<MyApp> {
             padding: const EdgeInsets.all(10),
             child: Column(
               children: [
-                const Text(
-                  'This calls a native function through FFI that is shipped as source in the package. '
-                  'The native code is built as part of the Flutter Runner build.',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
+                TextField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Text',
+                    hintText: "Enter your prompt here",
+                  ),
                 ),
-                spacerSmall,
+                const SizedBox(height: 10),
+                if (_encoded != null)
+                  Text(
+                    'Encoded: ${_encoded!.toList()}',
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                if (_decoded != null)
+                  Text(
+                    'Decoded: $_decoded',
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                if (_count != null)
+                  Text(
+                    'Count: $_count',
+                    style: const TextStyle(fontSize: 20),
+                  ),
               ],
             ),
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            if (tokenizer == null) return;
+
+            final text = _controller.text;
+            final encoded = await tokenizer!.encode(
+              text: text,
+              allowedSpecialEntries: const [],
+            );
+
+            final decoded = await tokenizer!.decodeBytes(tokens: encoded);
+
+            final count = await tokenizer!.countToken(
+              text: text,
+              allowedSpecialEntries: const [],
+            );
+
+            setState(() {
+              _encoded = encoded;
+              _decoded = utf8.decode(decoded);
+              _count = count;
+            });
+          },
+          child: const Text('Encode'),
         ),
       ),
     );
